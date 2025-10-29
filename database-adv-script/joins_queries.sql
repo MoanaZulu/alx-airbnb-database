921c7a7a58cbbb5ddf677289cfd2f776685c6f46
Moana <missn.hadebe@gmail.com>
2025-10-29 18:09:54 +0200

    Fix filename and add INNER JOIN query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,18 +1,14 @@
-
--- INNER JOIN: Bookings and Users
-SELECT b.id AS booking_id, u.name AS user_name
+-- INNER JOIN: Bookings with respective users
+SELECT b.*, u.*
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.id;
 
--- LEFT JOIN: Properties and Reviews
-SELECT p.id AS property_id, p.name, r.rating
+-- LEFT JOIN: Properties with reviews (including those without reviews)
+SELECT p.*, r.*
 FROM properties p
 LEFT JOIN reviews r ON p.id = r.property_id;
 
--- FULL OUTER JOIN: Users and Bookings
-SELECT u.name, b.id AS booking_id
+-- FULL OUTER JOIN: All users and bookings
+SELECT u.*, b.*
 FROM users u
 FULL OUTER JOIN bookings b ON u.id = b.user_id;
-
-
-
