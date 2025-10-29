4ed858b96dd60088900d5fa8e1024031959a7b6c
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-10-29 13:37:14 +0200

    Update joins_queries.sql

--- joins_queries.sql
+++ joins_queries.sql
@@ -1,27 +1,9 @@
-SELECT 
-    bookings.id AS booking_id,
-    bookings.start_date,
-    bookings.end_date,
-    users.id AS user_id,
-    users.name AS user_name,
-    users.email
+-- INNER JOIN: Bookings and Users
+SELECT bookings.id AS booking_id,
+       bookings.start_date,
+       bookings.end_date,
+       users.id AS user_id,
+       users.name AS user_name
 FROM bookings
 INNER JOIN users ON bookings.user_id = users.id;
 
-SELECT 
-    properties.id AS property_id,
-    properties.name AS property_name,
-    reviews.id AS review_id,
-    reviews.rating,
-    reviews.comment
-FROM properties
-LEFT JOIN reviews ON properties.id = reviews.property_id;
-
-SELECT 
-    users.id AS user_id,
-    users.name AS user_name,
-    bookings.id AS booking_id,
-    bookings.start_date,
-    bookings.end_date
-FROM users
-FULL OUTER JOIN bookings ON users.id = bookings.user_id;
