f5500fe92ed180c3ebd61a4a24b53e191a7475e9
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-10-29 16:12:12 +0200

    Delete joins_queries.sql

--- joins_queries.sql
+++
@@ -1,7 +0,0 @@
--- INNER JOIN: Retrieve all bookings and the respective users who made those bookings
-SELECT bookings.id AS booking_id,
-       users.id AS user_id,
-       users.name AS user_name
-FROM bookings
-INNER JOIN users ON bookings.user_id = users.id;
-
