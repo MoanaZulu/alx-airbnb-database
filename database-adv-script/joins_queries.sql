3c7a346f540b04cf7e9a5a95c6a0a6779f04f7ae
Moana <missn.hadebe@gmail.com>
2025-11-05 18:00:07 +0200

    Fix: recreate joins_queries.sql with correct INNER JOIN

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,25 +1,14 @@
-## joins_queries.sql
-
--- INNER JOIN: Bookings with their respective users
+-- INNER JOIN: Bookings and Users
 SELECT b.id AS booking_id, b.start_date, b.end_date, u.id AS user_id, u.name
 FROM Booking b
 INNER JOIN User u ON b.user_id = u.id;
 
--- LEFT JOIN: Properties and their reviews (including properties with no reviews)
+-- LEFT JOIN: Properties and Reviews
 SELECT p.id AS property_id, p.name AS property_name, r.id AS review_id, r.rating, r.comment
 FROM Property p
 LEFT JOIN Review r ON p.id = r.property_id;
 
--- FULL OUTER JOIN: All users and all bookings, even if not linked
+-- FULL OUTER JOIN: Users and Bookings
 SELECT u.id AS user_id, u.name, b.id AS booking_id, b.start_date, b.end_date
 FROM User u
 FULL OUTER JOIN Booking b ON u.id = b.user_id;
-
-SELECT p.id AS property_id, p.name AS property_name, r.id AS review_id, r.rating, r.comment
-FROM Property p
-LEFT JOIN Review r ON p.id = r.property_id;
-
-SELECT u.id AS user_id, u.name, b.id AS booking_id, b.start_date, b.end_date
-FROM User u
-FULL OUTER JOIN Booking b ON u.id = b.user_id;
-
