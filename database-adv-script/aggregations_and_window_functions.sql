12b8b4e775324c8fc4910eebe8e297921d8a4f7a
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-11-08 01:11:14 +0200

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,18 +1,15 @@
--- Total number of bookings made by each user
+-- Rank properties based on total number of bookings using ROW_NUMBER()
 SELECT
-    u.id AS user_id,
-    u.name AS user_name,
-    COUNT(b.id) AS total_bookings
-FROM users u
-JOIN bookings b ON u.id = b.user_id
-GROUP BY u.id, u.name;
-
--- Rank properties based on total number of bookings
-SELECT
-    p.id AS property_id,
-    p.name AS property_name,
-    COUNT(b.id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
-FROM properties p
-JOIN bookings b ON p.id = b.property_id
-GROUP BY p.id, p.name;
+    property_id,
+    property_name,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM (
+    SELECT
+        p.id AS property_id,
+        p.name AS property_name,
+        COUNT(b.id) AS total_bookings
+    FROM properties p
+    JOIN bookings b ON p.id = b.property_id
+    GROUP BY p.id, p.name
+) AS ranked_properties;
